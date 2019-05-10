116805ff4cce3247e9ca76233ddcf10f927e39fe
AlexanderLapygin <AlexanderLapygin@gmail.com>
2019-05-10 21:52:47 +0300

    fix: parameterized port

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ app.get('/', function(req, res) {
   res.send('Hello, world!');
 });
 
-app.listen(3000, function () {
-  console.log('Server listening at http://localhost:3000');
+const port = process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log('Server listening at http://localhost:' + port);
 });
